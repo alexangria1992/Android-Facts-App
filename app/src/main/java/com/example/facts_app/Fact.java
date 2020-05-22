b93e7e0b86fe0063d0cc0feded86df0484155d4b
alexangria1992 <55860445+alexangria1992@users.noreply.github.com>
2020-05-23 01:04:46 +1200

    Created Fact Class, upgraded SDK, turned of thing in settings of emulator

---
+++ app/src/main/java/com/example/facts_app/Fact.java
@@ -0,0 +1,20 @@
+package com.example.facts_app;
+
+public class Fact {
+    public static String[] getFacts(){
+
+        return new String[]{
+            "Animals that lay eggs don't have belly buttons.",
+                "A honey bee can fly at 15mph.",
+                "Steel is 100% recyclable.",
+                "There is a museum of strawberries in Belgium.",
+                "A day on Venus lasts longer than a year on Venus.",
+                "Queen Cleopatra of Egypt was not actually Egyptian.",
+                "The longest attack of hiccups ever lasted 68 years.",
+                "The name of the city of Portland, Oregon was decided by a coin toss. The name that lost was Boston.",
+                "Just a sugar cube of neutron star matter would weigh about one hundred million tons on Earth.",
+                "America's 1st roller coaster was built in 1827 to carry coal from a mine to boats below."
+
+        };
+    }
+}
--- app/src/main/java/com/example/facts_app/MainActivity.java
+++ app/src/main/java/com/example/facts_app/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.facts_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView factTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        factTextView = findViewById(R.id.txt_fact);
+        factTextView.setText("Morteza Kordi");
+
+
     }
 }
